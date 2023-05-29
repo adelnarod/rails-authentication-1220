6b5beb6ab21bc73d29aa77a4baaf7465041d8213
Adel Narod <adelnarod@gmail.com>
2023-05-29 10:01:46 +0400

    asohfud

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get 'about_us', to: 'pages#about_us'
+  # change
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
